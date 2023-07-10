05e759648152332d268ebf874502426f34cd98cb
Jota-T-S <jota_torres@hotmail.es>
2023-07-10 18:36:59 +0200

    Feat: API data storage with 1 hour interval

--- src/API/productsApi.jsx
+++ src/API/productsApi.jsx
@@ -1,10 +1,27 @@
 import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_APP_PRODUCTS_URL;
+const CACHE_TIME = 60 * 1000; // 1 hora en milisegundos
 
 export const getAllProducts = async () => {
   try {
+    const cachedData = localStorage.getItem(BASE_URL);
+    if (cachedData) {
+      const parsedData = JSON.parse(cachedData);
+      if (Date.now() - parsedData.timestamp < CACHE_TIME) {
+        return parsedData.data;
+      }
+    }
+
     const response = await axios.get(BASE_URL);
+    console.log(
+      "Solicitud a la API realizada:",
+      new Date().toLocaleTimeString("es-ES")
+    );
+    localStorage.setItem(
+      BASE_URL,
+      JSON.stringify({ data: response.data, timestamp: Date.now() })
+    );
     return response.data;
   } catch (error) {
     return error;
